e120671b88a4fbaf5295a66fcb21d4295eae1fac
leslie <leslie89757@126.com>
2015-04-24 14:35:51 +0800

    Initial Commit

---
+++ HexStringToByte/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HexStringToByte
+//
+//  Created by leslie on 15/4/24.
+//  Copyright (c) 2015年 leslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
